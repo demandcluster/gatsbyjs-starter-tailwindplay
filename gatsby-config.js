9d7f827645d067c5c1f7cbfcee7e788ee23a787c
demandJS_France <demandjs@sd-60174.dedibox.fr>
2020-08-05 05:45:18 +0200

    more tries at 100%

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+import heroImg from "../images/hero.png";
+
 module.exports = {
   siteMetadata: {
     title: `DemandCluster`,
@@ -48,7 +50,7 @@ module.exports = {
           ]
         },                                  // option to add more headers. `Link` headers are transformed by the below criteria
         allPageHeaders: [
-          "Link: </static/hero.png>; rel=preload; as=image",
+          `Link: <${heroImg}>; rel=preload; as=image`,
         ],                           // option to add headers for all pages. `Link` headers are transformed by the below criteria
         mergeSecurityHeaders: true,                   // boolean to turn off the default security headers
         mergeLinkHeaders: false,                      // boolean to turn off the default gatsby js headers (disabled by default, until gzip is fixed for server push)
