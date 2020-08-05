eb7df766da31320d6cd5485f1b784097b8cc841a
demandJS_France <demandjs@sd-60174.dedibox.fr>
2020-08-06 00:26:18 +0200

    cache

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,11 +12,11 @@ function Layout({ children }) {
             childImageSharp {
               # Specify the image processing specifications right in the query.
               # Makes it trivial to update as your page's design changes.
-              fixed(width: 125, height: 125) {
+              fixed(width: 250, height: 250) {
                 ...GatsbyImageSharpFixed
               }
             }
-          }
+          },
           site {
             siteMetadata {
               title,
