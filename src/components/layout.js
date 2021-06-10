715877abe5b5fde7591e9faa6f6675566dc66a25
rondlite <rondlite@gmail.com>
2021-06-10 07:03:13 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -80,7 +80,7 @@ function Layout({ children }) {
               <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
 
                 <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                  <p className="uppercase tracking-loose w-full">Next-Level eCommerce</p>
+                  <p className="uppercase tracking-loose w-full">Next-Level Developer-first eCommerce</p>
                   <h1 className="my-4 text-5xl font-bold leading-tight">DemandCluster</h1>
                   <p className="leading-normal text-2xl mb-8">
                    Rocket Science for Marketplaces
